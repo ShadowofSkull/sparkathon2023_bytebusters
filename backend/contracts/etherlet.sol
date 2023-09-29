1da2fa14bf7eadee1459dfd1b1e75509236e1d86
ShadowofSkull <zhplana@gmail.com>
2023-09-29 23:45:52 +0800

    compact the contracts & make points usable

--- backend/contracts/etherlet.sol
+++ backend/contracts/etherlet.sol
@@ -5,28 +5,14 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
-contract RewardPoints {
-    using SafeMath for uint256;
 
-    mapping(address => uint256) public rewardBalances;
-
-    // Function to check the reward balance of a user
-    function getRewardBalance(address user) external view returns (uint256) {
-        return rewardBalances[user];
-    }
-
-    // Function to reward a user with points
-    function rewardUser(address user) external {
-        uint256 points = 5;
-        rewardBalances[user] = rewardBalances[user].add(points);
-    }
-}
 
 contract NFTMarketplace is ERC721Enumerable, Ownable {
     using SafeMath for uint256;
 
     // Mapping from token ID to its price
     mapping(uint256 => uint256) private _tokenPrices;
+    mapping(address => uint256) public rewardBalances;
 
     event NFTMinted(address indexed owner, uint256 indexed tokenId);
     event TokenPriceSet(uint256 indexed tokenId, uint256 price);
@@ -34,12 +20,17 @@ contract NFTMarketplace is ERC721Enumerable, Ownable {
 
     constructor() ERC721("MyNFT", "MNFT") {}
 
+    // Function to check the reward balance of a user
+    function getRewardBalance(address user) public view returns (uint256) {
+        return rewardBalances[user];
+    }
+
     // Mint a new NFT
     function mintNFT(address to, uint256 tokenId) external onlyOwner {
         _mint(to, tokenId);
         emit NFTMinted(to, tokenId);
     }
-
+    // The price is set with the unit wei which is 10^-18 ether
     // Set the price for a specific NFT
     function setTokenPrice(uint256 tokenId, uint256 price) external onlyOwner {
         _tokenPrices[tokenId] = price;
@@ -51,22 +42,34 @@ contract NFTMarketplace is ERC721Enumerable, Ownable {
         return _tokenPrices[tokenId];
     }
 
+    // Function to reward a user with points
+    function rewardUser(address user) private {
+        uint256 points = 5;
+        rewardBalances[user] = rewardBalances[user] + points;
+    }
+
     // Purchase an NFT
-    function purchaseNFT(uint256 tokenId, address reward_addr) external payable {
+    function purchaseNFT(uint256 tokenId) external payable {
         uint256 price = _tokenPrices[tokenId];
         require(price > 0, "NFT not for sale");
-        require(msg.value >= price, "Insufficient funds");
-        
+
         address owner = ownerOf(tokenId);
         require(owner != address(0), "Invalid token");
-        
+
+        uint256 points = getRewardBalance(msg.sender);
+        uint256 rebatePoints = 10;
+        if(points < rebatePoints){
+            require(msg.value >= price, "Insufficient funds");
+            rewardUser(msg.sender);
+        }
+        else{
+
+            rewardBalances[msg.sender] = rewardBalances[msg.sender] - rebatePoints;
+        }
         // Transfer ownership and payment
         _transfer(owner, msg.sender, tokenId);
         payable(owner).transfer(msg.value);
 
         emit NFTPurchased(msg.sender, owner, tokenId, price);
-
-        RewardPoints rewardPoints = RewardPoints(reward_addr); 
-        rewardPoints.rewardUser(msg.sender);
     }
 }
